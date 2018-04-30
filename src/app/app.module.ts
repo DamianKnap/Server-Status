268f6dbcb794916eece9747483952dc742c89a89
Damian Knap <dknap@efigence.com>
2018-04-30 15:35:50 +0200

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Server } from 'selenium-webdriver/safari';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { ErrorMessageComponent } from './error-message/error-message.component';
+import { SuccesMessageComponent } from './succes-message/succes-message.component';
+import { WarningMessageComponent } from './warning-message/warning-message.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    ErrorMessageComponent,
+    SuccesMessageComponent,
+    WarningMessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
